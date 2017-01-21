465a9e389c948227dc8ca0c3b2a51ec5bb526dd5
lnishikawa <missnishikawa@gmail.com>
2017-01-21 12:51:11 -0500

    Basic Monsters, Managers, Spawn points

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour {
+
+    MonsterManager mgr = null;
+
+	// Use this for initialization
+	void Start () {
+        mgr = FindObjectOfType<MonsterManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour {
+
+    MonsterManager mgr = null;
+
+	// Use this for initialization
+	void Start () {
+        mgr = FindObjectOfType<MonsterManager>();
+        mgr.spawnPoints.Add(this);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
