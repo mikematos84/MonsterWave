d7682ac156ccae0afe62b4abab5d9fadf4db03ec
Michelle Shen <micshen@deloitte.com>
2017-01-21 20:12:33 -0500

    added low and high pitch calibration

---
+++ Assets/Scripts/MicrophoneInput/Calibration/CalibrationBase.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using VRTK.Examples;
+
+namespace MicrophoneInput
+{
+    public class CalibrationBase : MonoBehaviour
+    {
+
+        protected Text PitchCalibrationText;
+
+        protected const string PitchDetectionObjectName = "PitchDetection";
+
+        protected bool Calibrating;
+        public bool IsFinishedCalibrating { get; private set; }
+
+        protected string DetectingVoiceString;
+        protected string RequestVoiceString;
+        protected string RegisteredVoiceString;
+
+        protected PitchDetection PitchDetection;
+
+        protected float CalibrationTimer = 3f;
+
+
+        protected void Initialize()
+        {
+
+
+        }
+
+        protected void StartCalibration()
+        {
+            PitchCalibrationText.text = RequestVoiceString;
+            if (PitchDetection.PitchValue > 0)
+            {
+                Calibrating = true;
+            }
+        }
+
+
+
+        protected IEnumerator Calibrate()
+        {
+            var pitches = new List<float>();
+            float pitchTotal = 0f;
+
+            while (Calibrating && !IsFinishedCalibrating)
+            {
+                if(Calibrating)
+                    PitchCalibrationText.text = DetectingVoiceString;
+                if (PitchDetection.PitchValue > 0)
+                {
+                    pitches.Add(PitchDetection.PitchValue);
+                    pitchTotal += PitchDetection.PitchValue;
+                }
+                yield return new WaitForSeconds(CalibrationTimer);
+                Calibrating = false;
+                IsFinishedCalibrating = true;
+                var avgPitch = pitchTotal / pitches.Count;
+                PitchCalibrationText.text = RegisteredVoiceString + avgPitch;
+
+            }
+        }
+
+
+    }
+}
---
+++ Assets/Scripts/MicrophoneInput/Calibration/CalibrationManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MicrophoneInput.Calibration
+{
+    public class CalibrationManager : MonoBehaviour
+    {
+
+        private CalibrationBase _lowPitchCalibration;
+        private CalibrationBase _highPitchCalibration;
+
+        // Use this for initialization
+        void Start ()
+        {
+            _lowPitchCalibration = GameObject.Find("LowPitchCalibration").GetComponent<CalibrationBase>();
+            _highPitchCalibration = GameObject.Find("HighPitchCalibration").GetComponent<CalibrationBase>();
+
+            _lowPitchCalibration.gameObject.SetActive(false);
+            _highPitchCalibration.gameObject.SetActive(false);
+
+            _lowPitchCalibration.gameObject.SetActive(true);
+        }
+	
+        // Update is called once per frame
+        void Update () {
+
+            if (_lowPitchCalibration.IsFinishedCalibrating)
+            {
+                _highPitchCalibration.gameObject.SetActive(true);
+            }
+
+        }
+    }
+}
